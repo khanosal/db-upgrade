7572823104862f4bc1643557244f5a47704cbfb0
Salman Khan <salmankhan.it@gmail.com>
2018-11-01 19:31:11 +0000

    code tidy up added comments and updated readme removed extra files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # db-upgrade
-runs sql scripts in specified folder against a database 
+
+This script will run sql scripts in specified folder against a database after converting the first three characters into an integer value and comparing that with a version set in a versionTable
+
+Script should be executed one level above the directory specified by parameter:
+
+  --directory-with-sql-scripts
+
+The script runs in python 2.7 and works with mysql 5.7 it assumes you have the correct dependencies installed to import the following modules:
+
+os
+argparse
+mysql.connector
+mysql.connector import errorcode
+
+Run command:
+
+python upgrade.py
--- upgrade.py
+++ upgrade.py
@@ -5,21 +5,22 @@ from mysql.connector import errorcode
 
 # - './your-script.your-lang directory-with-sql-scripts username-for-the-db db-host db-name db-password'
 parser = argparse.ArgumentParser(description='Process database credentials and script locations')
-parser.add_argument('--directory_with_sql_scripts', required=True,
+parser.add_argument('--directory-with-sql-scripts', required=True,
                     help='directory which sql scripts are held')
 
-parser.add_argument('--username_for_the_db', required=True,
+parser.add_argument('--username-for-the-db', required=True,
                     help='db-username')
 
-parser.add_argument('--db_host', required=True,
+parser.add_argument('--db-host', required=True,
                     help='db-username')
 
-parser.add_argument('--db_name', required=True,
+parser.add_argument('--db-name', required=True,
                      help='db-username')
 
-parser.add_argument('--db_password', required=True,
+parser.add_argument('--db-password', required=True,
                     help='db-username')
 
+# parsing params and assigning them to variables
 args = parser.parse_args()
 sql_scripts_dir = args.directory_with_sql_scripts
 db_user = args.username_for_the_db
@@ -27,7 +28,7 @@ db_host = args.db_host
 db_name = args.db_name
 db_password = args.db_password
 
-
+# creating connection to mysql db using prams
 cnx = mysql.connector.connect(user=db_user, password=db_password,
                               host=db_host,
                               database=db_name,
@@ -35,38 +36,29 @@ cnx = mysql.connector.connect(user=db_user, password=db_password,
 
 cursor = cnx.cursor()
 
+# getting current version from versionTable and assigning it to variable version
 query = ("SELECT * FROM versionTable")
-
 cursor.execute(query)
-
 version = cursor.fetchone()[0]
-
 print 'current version = ',version
 
-# for (version) in cursor:
-#     print version
-
-cursor.close()
-
-
-
-
-# grabs sql_scripts_dir variable and prints ordered list of files
+# sorts then loops through all files in provided directory
 for filename in (sorted(os.listdir(sql_scripts_dir))):
+    # converts first three chars of file name to integer then compares with current version
     script_version = int(filename[:3])
     if script_version > version:
         print 'searching for newer version'
         os.chdir(sql_scripts_dir)
+        #parses then executes script from current file
         file = open(filename, 'r')
         sql = s = " ".join(file.readlines())
         cursor = cnx.cursor()
         print 'executing script',filename
         cursor.execute(sql)
-        cnx.commit()
-
+        # updates versionTable with new version
         update_statement = ("UPDATE versionTable SET version = %(version)s")
         cursor.execute(update_statement, { 'version': script_version })
-
+        cnx.commit()
         os.chdir("..")
         print 'updated current version to',script_version
 
